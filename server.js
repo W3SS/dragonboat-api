1b23f4ab801ba2f5db1969ff771ffa1d9910fb59
adrpadua <adrpadua@gmail.com>
2016-09-24 22:44:07 -0700

    cleaned up server code

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 
 var mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGODB_URI, (err, res) => {
   if (err) {
-    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+    console.log('ERROR connecting to: ' + process.env.MONGODB_URI + '. ' + err);
     process.exit(1);
   }
   else {
-    console.log('Succeeded connecting to: ' + uristring);
+    console.log('Succeeded connecting to: ' + process.env.MONGODB_URI);
 
     // START THE SERVER
     app.listen(port);
@@ -35,6 +36,21 @@ router.get('/', (req, res) => {
 });
 
 // more routes to follow
+router.route('/users')
+.post((req, res) => {
+  var user = new User();
+  user.name = req.body.name;
+
+  // save user info
+  user.save((err) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({
+      message : 'User Created!'
+    });
+  });
+});
 
 // REGISTER OUR routes
 // all of our routes prefixed with /api
